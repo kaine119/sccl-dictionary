581b9ea4b9c6aab3ac739c86f5f2654ab75bf6b5
Mui Kai En <muikaien1@gmail.com>
2018-04-10 20:48:33 +0800

    Add mdl styling to app - Add mdl packages - add material icons dep - Add styling to app shell - add back button to titlebar - add styling to search view - add styling to results 	- results now has a ResultItem component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,27 +1,50 @@
 <template>
   <div id="app">
-    <router-view/>
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+      <header class="mdl-layout__header">
+        <div class="mdl-layout-icon" v-show="notHome">
+          <button class="mdl-layout-icon mdl-button mdl-js-button mdl-button--icon"
+                  v-on:click="back">
+            <i class="material-icons">arrow_back</i>
+          </button>
+        </div>
+        <div class="mdl-layout__header-row">
+          <span class="mdl-layout-title">Dictionary</span>
+        </div>
+      </header>
+      <div class="mdl-layout__content">
+        <router-view/>
+      </div>
+    </div>
   </div>
 </template>
 
-<style>
+<script>
+require('mdl/material.min.css');
+require('material-icons/iconfont/material-icons.css');
+require('mdl');
+
+export default {
+  name: 'App',
+  computed: {
+    notHome() {
+      return this.$route.name !== 'search';
+    },
+  },
+  methods: {
+    back() {
+      this.$router.go(-1);
+    },
+  },
+};
+</script>
+
+<style scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /*text-align: center;*/
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
--- client/src/views/Search.vue
+++ client/src/views/Search.vue
@@ -1,8 +1,24 @@
 <template>
-  <input type="text" v-model="query" v-on:keyup.enter="search()">
+  <div class="mdl-grid">
+    <div class="mdl-cell mdl-cell--4-col mdl-cell--hide-phone"></div>
+    <div class="mdl-cell mdl-cell--8ol mdl-cell--4-col-phone" id="grid-cell">
+      <div class="mdl-card mdl-shadow--2dp" id="search-card">
+        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label mdl-shadow--" id="search-query">
+          <input id="query" type="text" class="mdl-textfield__input"
+                 v-model="query" v-on:keyup.enter="search()">
+          <label for="query" class="mdl-textfield__label">Search</label>
+        </div>
+        <button class="mdl-button mdl-js-button mdl-button--raised" v-on:click="search()">Search</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+// mdl stuff
+require('mdl/material.min.css');
+require('mdl');
+
 export default {
   name: 'Search',
   methods: {
@@ -13,5 +29,21 @@ export default {
   data: () => ({
     query: '',
   }),
+  mounted() {
+    /* eslint-disable no-undef */
+    componentHandler.upgradeElement(document.getElementById('search-query'));
+    /* eslint-enable */
+  },
 };
 </script>
+
+<style scoped>
+#search-query {
+  margin: auto;
+}
+#search-card {
+  width: 100%;
+  height: 50px;
+}
+
+</style>
